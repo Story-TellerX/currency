1640a08dcef447d82949f20c090b9cd60c768d88
Story-TellerX <ds.churikov85@gmail.com>
2021-06-02 01:52:10 +0300

    1. Exstations installed 2. sripts for gen data are create 3. Data was insert in t=a tables

---
+++ app/currency/management/commands/gen_data_contactus.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from currency.models import ContactUs
+from faker import Faker
+
+
+class Command(BaseCommand):
+    helps = 'Generate random records for contact us table'
+
+    def handle(self, *args, **options):
+        faker = Faker()
+        for index in range(100):
+            ContactUs.objects.create(
+                email_from=faker.unique.email(),
+                subject=faker.unique.sentence(nb_words=10, variable_nb_words=False),
+                message=faker.text(max_nb_chars=500),
+            )

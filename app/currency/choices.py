45830b7ded49525642ea9ba2e1b2f02a1b60411f
Story-TellerX <ds.churikov85@gmail.com>
2021-07-12 00:42:53 +0300

    Migration for int saving to db is added

---
+++ app/currency/choices.py
@@ -0,0 +1,7 @@
+RATE_TYPE_USD = 0  # 'USD'
+RATE_TYPE_EUR = 1  # 'EUR'
+
+RATE_TYPE_CHOICES = (
+    (RATE_TYPE_USD, 'Dollar'),
+    (RATE_TYPE_EUR, 'Euro'),
+)
--- app/currency/models.py
+++ app/currency/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.db.models import PositiveSmallIntegerField
+from currency import choices
 
 
 class Rate(models.Model):
-    type_curr = models.CharField(max_length=5)
+    type_curr = PositiveSmallIntegerField(choices=choices.RATE_TYPE_CHOICES)
     sale = models.DecimalField(max_digits=5, decimal_places=2)
     buy = models.DecimalField(max_digits=5, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)

1be4170a2d02a7eaead22abaf69a223fe9ccd243
Story-TellerX <ds.churikov85@gmail.com>
2021-07-12 23:09:54 +0300

    1. The model for analytics is created 2. Basic middleware is created

--- app/currency/models.py
+++ app/currency/models.py
@@ -36,3 +36,10 @@ class Bank(models.Model):
 
     def __str__(self):
         return f'Bank id: {self.id}'
+
+
+class AnalyticsLog(models.Model):
+    path = models.CharField(max_length=255)
+    counter = models.PositiveBigIntegerField()
+    request_method = PositiveSmallIntegerField(choices=choices.REQUEST_METHOD_CHOICES)
+    status_code = models.CharField(max_length=3)

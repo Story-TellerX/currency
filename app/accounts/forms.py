1062f431305e585f0ed06d41b297f4135f9d705b
Story-TellerX <ds.churikov85@gmail.com>
2021-07-20 16:23:51 +0300

    1. Signp model is refactored 2. SignUp form is refactored

--- app/accounts/forms.py
+++ app/accounts/forms.py
@@ -1,11 +1,30 @@
+import uuid
 from django import forms
 from accounts.models import User
 
 
 class SignUpForm(forms.ModelForm):
 
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
     class Meta:
         model = User
         fields = (
             'email',
+            'password1',
+            'password2',
         )
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        # instance.username = 'HELLO'
+        # instance.username = self.cleaned_data['email']
+        instance.username = str(uuid.uuid4())
+        instance.is_active = False
+
+        if commit:
+            instance.save()
+
+        return instance
--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,6 +1,13 @@
-# from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    email = models.EmailField(
+        'email address',
+        blank=False,
+        null=False,
+        unique=True,
+    )

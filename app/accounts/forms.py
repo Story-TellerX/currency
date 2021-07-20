57ac4c1fee46cdeaeb0c2132a6eae2d3c291d8ec
Story-TellerX <ds.churikov85@gmail.com>
2021-07-20 18:03:00 +0300

    1. Password1 and 2 validation is added 2. Sending email task is added

--- app/accounts/forms.py
+++ app/accounts/forms.py
@@ -1,6 +1,8 @@
 import uuid
 from django import forms
 from accounts.models import User
+from accounts.tasks import send_registration_email
+from django.conf import settings
 
 
 class SignUpForm(forms.ModelForm):
@@ -16,6 +18,18 @@ class SignUpForm(forms.ModelForm):
             'password2',
         )
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        return email.lower()
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if not self.errors:
+            if cleaned_data['password1'] != cleaned_data['password2']:
+                # self.add_error('password', 'Password do not match')
+                raise forms.ValidationError('Password do not match')
+        return cleaned_data
+
     def save(self, commit=True):
         instance = super().save(commit=False)
 
@@ -26,5 +40,13 @@ class SignUpForm(forms.ModelForm):
 
         if commit:
             instance.save()
+
+        body = f'''
+        Activate Your Account
+        {settings.DOMAIN}/activate/uuid
+        '''
+
+        send_registration_email.delay(body, self.cleaned_data['email'])
+        # email address can be taken from  instance.email
 
         return instance

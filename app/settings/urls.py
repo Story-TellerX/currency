f4b5ef104e2fd8ff1f97ef5db225cc845f0eca62
Story-TellerX <ds.churikov85@gmail.com>
2021-07-21 15:10:44 +0300

    1. Bootstrap is connected 2. Static dir is created and registered 3. Bootstrap css and jquery, bundle js is added

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from currency.views import IndexTemplateView
@@ -25,4 +27,4 @@ urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('auth/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

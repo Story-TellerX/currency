d1bd34fe3a582765d54978ded76636fde4519fa4
Story-TellerX <ds.churikov85@gmail.com>
2021-07-24 00:47:35 +0300

    Custom view for choices api is added

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,8 +1,13 @@
+from django.urls import path
 from rest_framework import routers
-from api.views import RateViewSets, BankViewSets
+from api.views import RateViewSets, BankViewSets, RateTypeChoicesView
 
 router = routers.DefaultRouter()
 router.register(r'rates', RateViewSets, basename='rate')
 router.register(r'banks', BankViewSets, basename='bank')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('choices/currency/types/', RateTypeChoicesView.as_view(), name='choices-currency-types'),
+]
+# urlpatterns = router.urls
+urlpatterns += router.urls
--- app/api/views.py
+++ app/api/views.py
@@ -1,7 +1,12 @@
+from rest_framework.response import Response
+
 from currency.models import Rate, Bank
 from api.serializers import RateSerializer, RateDetailsSerializer, BankSerializer, BankDetailsSerializer
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.views import APIView
+
+from currency import choices
 
 
 class RateList(generics.ListAPIView, generics.CreateAPIView):
@@ -37,3 +42,10 @@ class BankViewSets(viewsets.ModelViewSet):
         if 'pk' in self.kwargs:
             return BankDetailsSerializer
         return BankSerializer
+
+
+class RateTypeChoicesView(APIView):
+    def get(self, request, format=None):  # noqa
+        # return a list of all users
+        # usernames = [user.username in User.objects.all()]
+        return Response(choices.RATE_TYPE_CHOICES)

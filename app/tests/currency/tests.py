8bab07eafcdc2bca9f5632dbe2e897fb15407f43
Story-TellerX <ds.churikov85@gmail.com>
2021-08-03 20:59:59 +0300

    1. Fixtures is added 2. Basic test for form creation is added

--- app/tests/currency/tests.py
+++ app/tests/currency/tests.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 import pytest
 from currency import choices
+from currency.models import Bank, Rate
 
 
 def test_sanity_check():
@@ -55,14 +56,31 @@ def test_create_rate_empty_form(client):
 
 
 def test_create_rate_invalid_form(client):
+    rates_initial_counts = Rate.objects.count()
     form_data = {
         'type_curr': choices.RATE_TYPE_USD,
         'buy': 20,
         'sale': 30,
-        'bank': 1
+        'bank': 99999
     }
     response = client.post('/currency/rate/create/', data=form_data)
     assert response.status_code == 200
     assert response.context['form'].errors == {
         'bank': ['Select a valid choice. That choice is not one of the available choices.']
     }
+    assert Rate.objects.count() == rates_initial_counts
+
+
+def test_create_rate_success(client):
+    rates_initial_counts = Rate.objects.count()
+    bank = Bank.objects.first()
+    form_data = {
+        'type_curr': choices.RATE_TYPE_USD,
+        'buy': 20,
+        'sale': 30,
+        'bank': bank.id
+    }
+    response = client.post('/currency/rate/create/', data=form_data)
+    assert response.status_code == 302
+    assert response.url == '/currency/rate/list/'
+    assert Rate.objects.count() == rates_initial_counts + 1
